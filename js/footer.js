cd40acf42d135e31060306d4951bd279a799cdbf
Gabrielms2001 <3gms47@gmail.com>
2024-08-01 17:34:32 +0200

    aggiornamentoGraficiStats

--- js/footer.js
+++ js/footer.js
@@ -15,6 +15,7 @@ template.innerHTML =`
                         <li><a href="login.html" class="text-decoration-none">Accedi/Registrati</a></li>
                         <li><a href="statistiche.html" class="text-decoration-none">Statistiche</a></li>
                         <li><a href="consigli.html" class="text-decoration-none">Consigli</a></li>
+                        <li><a href="calcolatori.html" class="text-decoration-none">Calcolatore</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-4 col-md-4">
