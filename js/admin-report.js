076420b0e53cdc48182a704bbc4e93024b5c8827
StefanoMazzilli <stefano.mazzilli.1999@gmail.com>
2024-09-01 18:01:03 +0200

    fix grafici 2

--- js/admin-report.js
+++ js/admin-report.js
@@ -45,28 +45,28 @@ async function creazioneGrafici() {
         //controllo il periodo su cui dividere i dati
         if (periodo == 'Anno') {
             //controllo il tipo di dato da prendere
-            if (dato == 'calorie') {
+            if (dato=='calorie') {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getMonth() == contatore) {
-                        totale += attivita.calorie;
+                    if (date.getMonth()==contatore){
+                        totale+=attivita.calorie;
                     }
                 })
-            } else if (dato == 'durata') {
+            } else if (dato=='durata'){
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getMonth() == contatore) {
-                        totale += attivita.durata;
+                    if (date.getMonth()==contatore){
+                        totale+=attivita.durata;
                     }
                 })
-            } else if (dato == 'distanza') {
+            } else if (dato=='distanza'){
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getMonth() == contatore) {
-                        totale += attivita.distanza;
+                    if (date.getMonth()==contatore){
+                        totale+=attivita.distanza;
                     }
                 })
             }
@@ -75,24 +75,24 @@ async function creazioneGrafici() {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è dell'anno giusto
-                    if (date.getFullYear() == (parseInt(contatore) + parseInt(2020))) {
-                        totale += attivita.calorie;
+                    if (date.getFullYear()==(parseInt(contatore)+parseInt(2020))) {
+                        totale+=attivita.calorie;
                     }
                 })
             } else if (dato == 'durata') {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è dell'anno giusto
-                    if (date.getFullYear() == (parseInt(contatore) + parseInt(2020))) {
-                        totale += attivita.durata;
+                    if (date.getFullYear()==(parseInt(contatore)+parseInt(2020))) {
+                        totale+=attivita.durata;
                     }
                 })
             } else if (dato == 'distanza') {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è dell'anno giusto
-                    if (date.getFullYear() == (parseInt(contatore) + parseInt(2020))) {
-                        totale += attivita.distanza;
+                    if (date.getFullYear()==(parseInt(contatore)+parseInt(2020))) {
+                        totale+=attivita.distanza;
                     }
                 })
             }
@@ -101,24 +101,24 @@ async function creazioneGrafici() {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getDate() == (contatore + 1)) {
-                        totale += attivita.calorie;
+                    if (date.getDate()==(contatore+1)) {
+                        totale+=attivita.calorie;
                     }
                 })
             } else if (dato == 'durata') {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getDate() == (contatore + 1)) {
-                        totale += attivita.durata;
+                    if (date.getDate()==(contatore+1)) {
+                        totale+=attivita.durata;
                     }
                 })
             } else if (dato == 'distanza') {
                 array.forEach((attivita) => {
                     const date = new Date(attivita.sportDate);
                     //controllo se la data dell'attività è del mese giusto
-                    if (date.getDate() == (contatore + 1)) {
-                        totale += attivita.distanza;
+                    if (date.getDate()==(contatore+1)) {
+                        totale+=attivita.distanza;
                     }
                 })
             }
@@ -129,28 +129,28 @@ async function creazioneGrafici() {
     //creo il PRIMO dei tre grafici
     async function createChart1(dato, periodo) {
         //definisco il periodo
-        const titleChart = "Confronto delle attività per la media di: " + dato;
+        const titleChart = "Confronto delle attività per la media di: "+dato;
         let xValues = [];
         const oggi = new Date();
         if (periodo == 'Totale') {
             const anno = oggi.getFullYear();
             //faccio aggiungere gli anni dal 2020 all'anno attuale
-            for (j = 2020; j <= anno; j++) {
+            for (j=2020; j<=anno; j++) {
                 xValues.push(j);
             }
         } else if (periodo == 'Anno') {
-
+            
             xValues = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
         } else {
             //costruisco l'array dei giorni del mese considerando che mese è e se l'anno è bisestile
             const anno = parseInt(oggi.getFullYear());
             const mese = parseInt(oggi.getMonth());
-            if ((mese == 0) || (mese == 2) || (mese == 4) || (mese == 6) || (mese == 7) || (mese == 9) || (mese == 11)) {
+            if ((mese==0)||(mese==2)||(mese==4)||(mese==6)||(mese==7)||(mese==9)||(mese==11)) {
                 //mese da 31 giorni
                 xValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
-            } else if (mese == 1) {
+            } else if (mese==1) {
                 //febbraio
-                if (anno % 4 == 0) {
+                if (anno%4==0) {
                     //anno bisestile
                     xValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29'];
                 } else {
@@ -186,11 +186,20 @@ async function creazioneGrafici() {
             sumCorsa = organiseData(periodo, arrayCorsa, dato, i);
             sumNuoto = organiseData(periodo, arrayNuoto, dato, i);
             //media
-            valueCiclismo[i] = (sumCiclismo / arrayCiclismo.length).toFixed(2);
-            valueCorsa[i] = (sumCorsa / arrayCorsa.length).toFixed(2);
-            valueNuoto[i] = (sumNuoto / arrayNuoto.length).toFixed(2);
+            valueCiclismo[i] = (sumCiclismo/arrayCiclismo.length).toFixed(2);
+            valueCorsa[i] = (sumCorsa/arrayCorsa.length).toFixed(2);
+            valueNuoto[i] = (sumNuoto/arrayNuoto.length).toFixed(2);
         }
         //creo il diagramma
+        //controllo le dimensioni schermo
+        var larghezza = $(window).width();
+        var CHECK;
+        if (larghezza < 500) {
+            CHECK = false;
+        } else {
+            CHECK = true;
+        }
+        console.log(CHECK);
         new Chart("attivita-tempo", {
             type: "line",
             data: {
@@ -200,12 +209,12 @@ async function creazioneGrafici() {
                     data: valueCiclismo,
                     borderColor: "red",
                     fill: false
-                }, {
+                },{
                     label: 'Corsa',
                     data: valueCorsa,
                     borderColor: "blue",
                     fill: false
-                }, {
+                },{
                     label: 'Nuoto',
                     data: valueNuoto,
                     borderColor: "green",
@@ -216,19 +225,20 @@ async function creazioneGrafici() {
                 title: {
                     display: true,
                     text: titleChart
-                }
+                },
+                responsive: CHECK
             }
         });
     }
     //aggiungo un evento alla selezione delle opzioni al diagramma uno
     const selettoreUno = document.getElementById('selectChartUno');
-    selettoreUno.addEventListener('change', function () {
+    selettoreUno.addEventListener('change' , function() {
         const buttonChartUno = document.getElementsByClassName('periodoUno');
         let periodo;
         for (let pulsante of buttonChartUno) {
             if (pulsante.classList.contains('selected')) {
                 //trovo il periodo selezionato
-                periodo = pulsante.innerText;
+                periodo=pulsante.innerText;
             }
         }
         createChart1(this.value, periodo);
@@ -236,7 +246,7 @@ async function creazioneGrafici() {
     //aggiungo un evento ai pulsanti del periodo per il diagramma uno
     const periodoUno = document.getElementsByClassName('periodoUno');
     for (let button of periodoUno) {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             for (let pulsante of periodoUno) {
                 //rimuovo la classe selected da tutti i pulsanti
                 pulsante.classList.remove('selected');
@@ -257,25 +267,28 @@ async function creazioneGrafici() {
         //definisco il periodo
         let xValues = [];
         const oggi = new Date();
+        let chartBgColor;
         if (periodo == 'Totale') {
             const anno = oggi.getFullYear();
             //faccio aggiungere gli anni dal 2020 all'anno attuale
-            for (j = 2020; j <= anno; j++) {
+            for (j=2020; j<=anno; j++) {
                 xValues.push(j);
             }
+            //sistemare colori anni
+            chartBgColor = ['red', 'blue', 'green', 'yellow', 'orange'];
         } else if (periodo == 'Anno') {
-
+            chartBgColor = ['red', 'blue', 'green', 'yellow', 'orange', 'brown', 'black', 'violet', 'white', 'gray'];
             xValues = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
         } else {
             //costruisco l'array dei giorni del mese considerando che mese è e se l'anno è bisestile
             const anno = parseInt(oggi.getFullYear());
             const mese = parseInt(oggi.getMonth());
-            if ((mese == 0) || (mese == 2) || (mese == 4) || (mese == 6) || (mese == 7) || (mese == 9) || (mese == 11)) {
+            if ((mese==0)||(mese==2)||(mese==4)||(mese==6)||(mese==7)||(mese==9)||(mese==11)) {
                 //mese da 31 giorni
                 xValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
-            } else if (mese == 1) {
+            } else if (mese==1) {
                 //febbraio
-                if (anno % 4 == 0) {
+                if (anno%4==0) {
                     //anno bisestile
                     xValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29'];
                 } else {
@@ -313,34 +326,60 @@ async function creazioneGrafici() {
             //somma totale
             valueTot[i] = sumCiclismo + sumCorsa + sumNuoto;
         }
-        //creo il secondo diagramma
-        new Chart('tot-attivita', {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    label: labelChart,
-                    borderColor: "white",
-                    data: valueTot
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: titleChart
-                }
+        //controllo le dimensioni schermo
+        var larghezza = $(window).width();
+        //creo il secondo diagramma a seconda dello schermo
+        if (larghezza < 500) {
+            if (periodo != "Mese") {
+                new Chart ('tot-attivita', {
+                    type: "doughnut",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                            backgroundColor: chartBgColor,
+                            data: valueTot
+                        }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: titleChart
+                        },
+                        legend: {display: false}
+                    }
+                });
+            } else {
+                alert('FUNZIONE NON ANCORA DISPONIBILE');
             }
-        })
+        } else {
+            new Chart ('tot-attivita', {
+                type: "line",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        label: labelChart,
+                        borderColor: "white",
+                        data: valueTot
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: titleChart
+                    }
+                }
+            })
+        }
     }
     //aggiungo un evento alla selezione delle opzioni al diagramma due
     const selettoreDue = document.getElementById('selectChartDue');
-    selettoreDue.addEventListener('change', function () {
+    selettoreDue.addEventListener('change' , function() {
         const buttonChartDue = document.getElementsByClassName('periodoDue');
         let periodo;
         for (let pulsante of buttonChartDue) {
             if (pulsante.classList.contains('selected')) {
                 //trovo il periodo selezionato
-                periodo = pulsante.innerText;
+                periodo=pulsante.innerText;
             }
         }
         createChart2(this.value, periodo);
@@ -348,7 +387,7 @@ async function creazioneGrafici() {
     //aggiungo un evento ai pulsanti del periodo per il diagramma due
     const periodoDue = document.getElementsByClassName('periodoDue');
     for (let button of periodoDue) {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             for (let pulsante of periodoDue) {
                 //rimuovo la classe selected da tutti i pulsanti
                 pulsante.classList.remove('selected');
@@ -367,7 +406,7 @@ async function creazioneGrafici() {
         const titleChart = "Numero di utenti per attività";
         //definisco le tre attività in esame
         let xValues = ["Ciclismo", "Corsa", "Nuoto"];
-
+               
         //prendo le attività per ogni singola attività
         let arrayCiclismo = [];
         arrayCiclismo = await fetchAttivita(periodo, 'cycling');
@@ -377,7 +416,7 @@ async function creazioneGrafici() {
         arrayNuoto = await fetchAttivita(periodo, 'swimming');
         let yValues = [arrayCiclismo.length, arrayCorsa.length, arrayNuoto.length];
         //adesso creo il terzo grafico
-        new Chart('attivita-numero', {
+        new Chart ('attivita-numero', {
             type: "doughnut",
             data: {
                 labels: xValues,
@@ -397,7 +436,7 @@ async function creazioneGrafici() {
     //creo un evento per i tre pulsanti
     const periodoTre = document.getElementsByClassName('periodoTre');
     for (let button of periodoTre) {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             const periodo = this.innerText;
             createChart3(periodo);
         })
@@ -407,9 +446,9 @@ async function creazioneGrafici() {
     createChart3('Anno');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     creazioneGrafici();
 });
-window.addEventListener('resize', function () {
+/*window.addEventListener('resize', function() {
     creazioneGrafici();
-})
+})*/
