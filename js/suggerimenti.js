5d48a39d221372fa793b923469509f11692031c7
Daniel23023 <danielsamsonsd23@gmail.com>
2024-09-02 10:57:08 +0200

    aggiunta controllo

--- js/suggerimenti.js
+++ js/suggerimenti.js
@@ -247,3 +247,26 @@ function updateNotifications(cardioAtt) {
     document.getElementById("cardiostabile2").style.display = "none";
   }
 }
+
+
+async function controlloAccesso() {
+  const token = localStorage.getItem("authToken");
+  //controllo se il token è associato a un utente
+  try {
+    const response = await fetch(`http://localhost:8080/utenti/me`, {
+      method: "GET",
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    if (response.status == 200) {
+      alert("Benvenuto utente");
+    } else {
+      alert("Accesso non concesso. Fai prima il log-in!");
+      window.location.href = "./login.html";
+    }
+  } catch (errore) {
+    alert("Problema di connessione. Riprova più tardi");
+  }
+}
+document.addEventListener("DOMContentLoaded", controlloAccesso());
